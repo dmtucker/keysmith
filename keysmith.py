a693dc8ba71893412178bf5df959439e71e13c7d
David Tucker <dmtucker@users.noreply.github.com>
2015-07-03 15:51:49 -0700

    Objectify.

--- keysmith.py
+++ keysmith.py
@@ -1,44 +1,49 @@
 #!/usr/bin/env python
 
+
 import argparse
 import os
 import random
 import sys
 
 
-def natural_number(x):
-    if x != int(x) or x < 0:
-        raise argparse.ArgumentTypeError(str(x) + ' is not a natural number.')
-    return x
+class Keysmith:
 
+    words = None
 
-def random_word(words):
-    """ Generate a random word. """
-    return random.SystemRandom().choice(words).strip()
+    def __init__(self, words):
+        self.words = words
 
-
-def random_key(words, degree):
-    """ Generate a random key. """
-    key = ''
-    for i in range(degree):
-        key += random_word(words)
-    return key
+    def generate(self, degree):
+        key = ''
+        for i in range(degree):
+            key += random.SystemRandom().choice(self.words).strip()
+        return key
 
 
 if __name__ == '__main__':
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         '-d', '--degree',
         help='Specify the number of words to include.',
-        type=natural_number,
+        type=int,
         default=3
     )
     parser.add_argument(
-        '-s', '--source',
+        '-q', '--quantity',
+        help='Specify the number of keys to generate.',
+        type=int,
+        default=1
+    )
+    parser.add_argument(
+        '-w', '--words',
         help='Specify the word list to use.',
         default=os.path.join(os.path.dirname(sys.argv[0]), 'word.list')
     )
     args = parser.parse_args()
-    with open(args.source, 'r') as source:
-        words = source.readlines()
-    print(random_key(words, args.degree))
+
+    with open(args.words, 'r') as f:
+        words = f.readlines()
+    for i in range(args.quantity):
+        print(Keysmith(words).generate(args.degree))
