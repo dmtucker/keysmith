f0f0999e307ebe9256eeeae7745f6bf051d80184
David Tucker <dmtucker@users.noreply.github.com>
2015-07-03 15:01:07 -0700

    Fix natural number validation.

--- keysmith.py
+++ keysmith.py
@@ -1,36 +1,36 @@
 #!/usr/bin/env python
 
-from __future__ import print_function
 import argparse
 import os
 import random
 import sys
 
-def natural_int(x):
-    x = int(x)
-    if x < 0:
+
+def natural_number(x):
+    if x != int(x) or x < 0:
         raise argparse.ArgumentTypeError(str(x) + ' is not a natural number.')
     return x
 
+
 def random_word(words):
     """ Generate a random word. """
-    return random.SystemRandom().choice(words)
+    return random.SystemRandom().choice(words).strip()
+
 
 def random_key(words, degree):
     """ Generate a random key. """
     key = ''
     for i in range(degree):
-        key += random_word(words).strip()
+        key += random_word(words)
     return key
 
+
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Authentication Token Generator'
-    )
+    parser = argparse.ArgumentParser()
     parser.add_argument(
         '-d', '--degree',
         help='Specify the number of words to include.',
-        type=natural_int,
+        type=natural_number,
         default=3
     )
     parser.add_argument(
@@ -42,4 +42,3 @@ if __name__ == '__main__':
     with open(args.source, 'r') as source:
         words = source.readlines()
     print(random_key(words, args.degree))
-
