cf370fa6ef2ddde31936da5e7ca8cb6b41331338
dtucker <david.michael.tucker@gmail.com>
2015-12-20 23:56:39 -0800

    Remove the long description from the package.

--- setup.py
+++ setup.py
@@ -6,15 +6,11 @@ import setuptools
 
 from keysmith import __version__
 
-with open('README.md', 'r') as f:
-    long_description = f.read()
-
 setuptools.setup(
     name='keysmith',
     version=__version__,
     license='GPL',
     description='Diceware-style Password Generator',
-    long_description=long_description,
     url='https://github.com/dmtucker/keysmith-py',
     packages=['keysmith'],
     package_data={'keysmith': ['words.txt']},
