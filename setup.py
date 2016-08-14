9c3bc4f2a141502b4121109d814fc18e90974744
David Tucker <david@tucker.name>
2016-08-13 17:02:28 -0700

    Bump the Development Status to Stable

--- setup.py
+++ setup.py
@@ -35,6 +35,6 @@ setup(
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
     ],
 )
