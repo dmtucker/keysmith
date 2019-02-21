cb0747417525d23614d600533dc453a57049eb01
David Tucker <david@tucker.name>
2019-02-20 22:30:34 -0800

    Add test_setup.py

---
+++ test_setup.py
@@ -0,0 +1,20 @@
+"""Test setup.py."""
+
+
+import os
+import subprocess
+import sys
+
+
+def test_setup():
+    """Run setup.py check."""
+    command = [sys.executable, 'setup.py', 'check', '--metadata', '--strict']
+    assert subprocess.run(command).returncode == 0
+
+
+def test_console_scripts():
+    """Ensure console scripts were installed correctly."""
+    assert any(
+        os.path.isfile(os.path.join(directory, 'keysmith'))
+        for directory in sys.path
+    )
