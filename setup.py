14537e249fcdb825b4db55dfdd72101e276c79b9
David Tucker <david@tucker.name>
2019-02-20 22:06:30 -0800

    Remove redundant trove classifiers

--- setup.py
+++ setup.py
@@ -44,12 +44,5 @@ setuptools.setup(
     py_modules=['keysmith'],
     entry_points={'console_scripts': ['keysmith=keysmith:main']},
     keywords='diceware generator keygen passphrase password',
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'License :: OSI Approved'
-        ' :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-    ],
+    classifiers=['Development Status :: 5 - Production/Stable'],
 )
